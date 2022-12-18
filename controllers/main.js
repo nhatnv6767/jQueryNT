38019312358c38b1bdf275b8151f8075ee1a1238
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-19 02:13:06 +0900

    render model after click thu do 9.3

--- controllers/main.js
+++ controllers/main.js
@@ -161,32 +161,118 @@ $(document).ready(function () {
 
     })
 
-    function renderContain(chosenItems) {
-        if (chosenItems && chosenItems.length > 0) {
-            console.log(chosenItems)
-            chosenItems.forEach(function (item) {
-                if (item.type === "topclothes") {
 
-                }
-                if (item.type === "botclothes") {
-
-                }
-                if (item.type === "shoes") {
-
-                }
-                if (item.type === "handbags") {
-
-                }
-                if (item.type === "necklaces") {
-
-                }
-                if (item.type === "hairstyle") {
-
-                }
-                if (item.type === "background") {
-
-                }
-            })
-        }
-    }
 })
+
+function renderContain(chosenItems) {
+    if (chosenItems && chosenItems.length > 0) {
+        console.log(chosenItems)
+        chosenItems.forEach(function (item) {
+            if (item.type === "topclothes") {
+                renderBikiniTop(item.imgsrc_png);
+            }
+            if (item.type === "botclothes") {
+                renderBikiniBottom(item.imgsrc_png);
+            }
+            if (item.type === "shoes") {
+                renderFeet(item.imgsrc_png);
+            }
+            if (item.type === "handbags") {
+                renderHandbags(item.imgsrc_png);
+            }
+            if (item.type === "necklaces") {
+                renderNecklace(item.imgsrc_png);
+            }
+            if (item.type === "hairstyle") {
+                renderHairstyle(item.imgsrc_png);
+            }
+            if (item.type === "background") {
+                renderBackground(item.imgsrc_png);
+            }
+        })
+    }
+}
+
+function renderBikiniTop(img) {
+    $(".bikinitop").css({
+        width: "500px",
+        height: "500px",
+        background: `url(${img})`,
+        position: "absolute",
+        top: "-9%",
+        left: "-5%",
+        zIndex: "3",
+        transform: "scale(0.5)"
+    });
+}
+
+function renderBikiniBottom(img) {
+    $(".bikinibottom").css({
+        width: "500px",
+        height: "1000px",
+        background: `url(${img})`,
+        position: "absolute",
+        top: "-30%",
+        left: "-5%",
+        zIndex: "2",
+        transform: "scale(0.5)"
+    });
+}
+
+function renderFeet(img) {
+    $(".feet").css({
+        width: "500px",
+        height: "1000px",
+        background: `url(${img})`,
+        position: "absolute",
+        bottom: "-37%",
+        right: "-3.5%",
+        transform: "scale(0.5)",
+        zIndex: "1"
+    });
+}
+
+function renderHandbags(img) {
+    $(".handbag").css({
+        width: "500px",
+        height: "1000px",
+        background: `url(${img})`,
+        position: "absolute",
+        bottom: "-40%",
+        right: "-3.5%",
+        transform: "scale(0.5)",
+        zIndex: "4"
+    });
+}
+
+function renderNecklace(img) {
+    $(".necklace").css({
+        width: "500px",
+        height: "1000px",
+        background: `url(${img})`,
+        position: "absolute",
+        bottom: "-40%",
+        right: "-3.5%",
+        transform: "scale(0.5)",
+        zIndex: "4"
+    });
+}
+
+function renderHairstyle(img) {
+    $(".hairstyle").css({
+        width: "1000px",
+        height: "1000px",
+        background: `url(${img})`,
+        position: "absolute",
+        top: "-75%",
+        right: "-57%",
+        transform: "scale(0.15)",
+        zIndex: "4"
+    });
+}
+
+function renderBackground(img) {
+    $(".background").css({
+        backgroundImage: `url(${img})`
+    });
+}
