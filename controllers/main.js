fcd92ae010cca955661478a136b83c7162d3c1c2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-19 01:01:04 +0900

    render tabPane 2

--- controllers/main.js
+++ controllers/main.js
@@ -8,12 +8,21 @@ $(document).ready(function () {
             .done(function (result) {
                 console.log(result.navPills)
                 var contentNavPills = ""
+                var contentTabPanes = ""
                 result.navPills.forEach(function (item, index) {
                     var activeClass = item.tabName === "tabTopClothes" ? "active" : ""
 
                     contentNavPills += getElmTabPill(item, activeClass)
+                    contentTabPanes += `
+                        <div class="tab-pane container active" id="tabTopClothes">
+                            <div class="row">
+                                ${item.showName}
+                            </div>
+                        </div>
+                    `
                 })
                 $(".nav-pills").html(contentNavPills)
+                // $(".tab-content").html(contentTabPanes)
             })
             .fail(function (err) {
                 console.log(err)
