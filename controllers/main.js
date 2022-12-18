46a419ebdaadf420c66843ff3b14ee50e6709e06
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-19 00:34:17 +0900

    main js call data from CallDataJS 2

--- controllers/main.js
+++ controllers/main.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
     var callData = new CallData();
 
     function renderHTML() {
-        callData.getListData();
+        callData.getListData()
+            .done(function (result) {
+                console.log(result)
+            })
+            .fail(function (err) {
+                console.log(err)
+            });
     }
 })
